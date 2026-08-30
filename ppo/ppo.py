754f1244ec2001bc1cb4283fce81fd2667233e0a
Allen Abraham <allenabraham106@gmail.com>
2026-08-29 21:43:20 -0600

    starting PPO updates

--- ppo/ppo.py
+++ ppo/ppo.py
@@ -1,4 +1,6 @@
 import numpy as np
+import torch
+import torch.optim as optim
 
 def compute_gae(rewards, values, dones, last_value, gamma=0.09, lam=0.95):
     advantages = np.zeros_like(rewards)
@@ -18,3 +20,14 @@ def compute_gae(rewards, values, dones, last_value, gamma=0.09, lam=0.95):
 
     returns = advantages + values
     return returns, advantages
+
+class PPO: 
+    def __init__(self, policy, lr=3e-4, clip_eps=0.2, epochs=10, minibatch_size=64, value_coef=0.5, entropy_coef=0.01):
+        self.policy = policy
+        self.optimizer = optim.Adam(policy.parameters(), lr=lr)
+        self.clips_eps = clip_eps
+        self.epochs = epochs
+        self.minibatch_size = minibatch_size
+        self.value_coef = value_coef
+        self.entropy_coef = entropy_coef
+
